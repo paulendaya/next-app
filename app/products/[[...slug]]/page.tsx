933fde5f9a64b8fdc007420dddde55e8fa9da613
Paul Endaya <paul@boxonvision.com>
2025-07-31 10:00:54 +0300

    Lesson: added searchParams to products page

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 interface Props {
-  params: { slug: string[] };
+  params: { slug: string[] }; // params is an object that contains the slug parameter and other parameters
+  searchParams: { sortOrder: string };
 }
 
-const page = ({ params: { slug } }: Props) => {
-  return <div>Product Page {slug?.join(" ")}</div>;
+const page = ({ params: { slug }, searchParams: { sortOrder } }: Props) => {
+  return (
+    <div>
+      Product Page {slug?.join(" ")} {sortOrder}
+    </div>
+  );
 };
 
 export default page;
