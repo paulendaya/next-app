7743519a42ecfc141afd91c49fa4564fda442f40
Paul Endaya <paul@boxonvision.com>
2025-08-03 11:10:07 +0300

    Lesson: commented on loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return <div className="loading loading-spinner loading-md"></div>;
+  //Easier way to implement loading UI in next JS
 };
 
 export default Loading;
---
+++ app/users/loading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const UsersLoading = () => {
+  return <div>Loading Users...</div>;
+  //Easier way to implement loading UI in different pages in next JS
+};
+
+export default UsersLoading;
