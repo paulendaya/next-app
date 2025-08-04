0d5400c7c611a7d7a26f1bd4eb70d009699fba20
Paul Endaya <paul@boxonvision.com>
2025-08-04 09:32:33 +0300

    Lesson: amended API single object - to use inline types

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 interface Props {
   params: { id: number };
 }
+// export function GET(req: NextRequest, { params: { id } }: Props) { //use this if you want inline types
 export function GET(req: NextRequest, { params }: Props) {
   if (params.id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
