bf23e3150038465aa45d0047a64845d22bde3849
Paul Endaya <paul@boxonvision.com>
2025-08-04 09:30:30 +0300

    Lesson: finished API getting a single object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -3,8 +3,7 @@ interface Props {
   params: { id: number };
 }
 export function GET(req: NextRequest, { params }: Props) {
-    if (params.id > 10) return notFound();
-  return NextResponse.json([
-    { id: 1, name: "John Doe", email: "john@doe.com" },
-  ]);
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  return NextResponse.json({ id: 1, name: "John Doe", email: "john@doe.com" });
 }
