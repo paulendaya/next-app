6319999671790d29e9de2f8e4b171df6d047f0d9
paulendaya-Boxon <endaya.paul@gmail.com>
2025-08-20 16:14:23 +0300

    Lesson: Prisma Create User

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -3,6 +3,7 @@
 
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
+import schema from "./[id]/schema";
 //GET - getting data
 //POST - creating data
 //PUT - updating data
@@ -25,8 +26,8 @@ export async function GET(req: NextRequest) {
   return NextResponse.json(users);
 }
 
-//Create
-export async function POST(req: NextRequest) {
+//Create - Hardcoded
+/* export async function POST(req: NextRequest) {
   const body = await req.json();
   //Validate
   if (!body.name || !body.email)
@@ -38,4 +39,34 @@ export async function POST(req: NextRequest) {
   const newBody = { id: Math.random(), ...body }; //...body means we're copying the object since we can't mutate it
   //Return the response
   return NextResponse.json(newBody, { status: 201 }); //201 - Created Test comment
+} */
+
+//Create - Database
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  //Validate
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(
+      { error: validation.error.errors },
+      { status: 400 }
+    );
+
+  //check if user already exists
+  const user = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+  if (user)
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+
+  //add to the database
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  //Return the response
+  return NextResponse.json(newUser, { status: 201 });
 }
