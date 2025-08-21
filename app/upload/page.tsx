2dcd9185a5852c4b3c49dd08282810516a597a37
paulendaya-Boxon <endaya.paul@gmail.com>
2025-08-21 10:35:57 +0300

    Lesson: Cloudinary - Show uploaded images

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,16 +1,42 @@
 "use client";
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
+
+//type the result of the upload: for now only public_id, because currently its not properly typed
+interface CloudinaryResult {
+  public_id: string;
+  width: number;
+  height: number;
+}
 
 const UploadPage = () => {
+  const [resource, setResource] = useState<any>("");
   return (
-    <CldUploadWidget uploadPreset="cld-nxtjs-23112">
-      {({ open }) => (
-        <button className="btn btn-primary" onClick={() => open()}>
-          Upload
-        </button>
+    <>
+      {resource && (
+        <CldImage
+          width={resource.width}
+          height={resource.height}
+          src={resource.public_id}
+          alt={"An Image"}
+        />
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        uploadPreset="cld-nxtjs-23112"
+        onSuccess={(result, { widget }) => {
+          if (result.event !== "success") return;
+          //widget.close();
+          const info = result.info as CloudinaryResult; //assign the result.info as CloudinaryResult using assertion
+          setResource(info);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 
