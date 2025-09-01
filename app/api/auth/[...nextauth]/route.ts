fe354c5f8fff1519fe01922c7f66eed7e112d0fc
Boxon-BH <subscriptions@boxonvision.com>
2025-09-01 09:53:26 +0300

    Lesson: Auth - accessing session on the server

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,16 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
+export const authOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!, //the exclamation mark is added to tell typescript we do have this value at compile time
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
     ],
-});
+}
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
 //we're telling next-auth to use the /api/auth/[...nextauth] route for all the auth endpoints
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import ProductCart from "./components/ProductCard/ProductCart";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     //Client side navigation
     // All components inside the app folder are server components by default
     <main>
-      <h1>Hello World</h1>
+      <h1>Hello World {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
       <ProductCart />
     </main>
