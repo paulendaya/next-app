1db50773e584a01f408393bf4bccddafbb8555a2
Paul Endaya <endaya.paul@gmail.com>
2025-09-03 00:06:00 +0300

    Lesson: Auth - Database Adapters

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,21 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
 
-export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!, //the exclamation mark is added to tell typescript we do have this value at compile time
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-    ],
-}
+export const authOptions: NextAuthOptions = {
+  // :NextAuthOptions is a type that defines the options for the next-auth library
+  adapter: PrismaAdapter(prisma), //we're using the prisma adapter to store the user in the database
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!, //the exclamation mark is added to tell typescript we do have this value at compile time
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: "jwt", //jwt is the default strategy, this fixes the signing error that says "Try signing in with a different account"
+  },
+};
 
 const handler = NextAuth(authOptions);
 
