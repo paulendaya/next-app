b6a87d3b0275f3a140b44b082ddc00fd214d6867
Paul Endaya <endaya.paul@gmail.com>
2025-09-25 07:34:27 +0300

    added reset password on next-auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,14 @@
 //this is a middleware file that we use to protect certain routes
 //for example, we don't want to show the admin dashboard to users who are not authenticated
-export { default } from 'next-auth/middleware'
+export { default } from "next-auth/middleware";
 
 //we are exporting the default middleware from next-auth/middleware
 //we are also using the matcher function to protect certain routes
 
 export const config = {
-    // *: means zero or more
-    // +: means one or more
-    // ?: means zero or one
-  matcher: ["/dashboard/:path*"],
-}
+  // *: means zero or more
+  // +: means one or more
+  // ?: means zero or one
+  matcher: ["/dashboard/:path*", "/auth/reset-password:path*"],
+};
 //config is an object that contains the matcher function
