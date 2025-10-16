d6096abe194c846a13dd510e7f0c6a29b4b3f192
Boxon-BH <subscriptions@boxonvision.com>
2025-10-16 11:27:35 +0300

    Lesson: setup WelcomeTemplate email with Tailwind

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { CSSProperties } from "react";
+import { Html } from "@react-email/html";
+import {
+  Body,
+  Container,
+  Tailwind,
+  Text,
+  Link,
+  Preview,
+} from "@react-email/components";
 
-const WelcomeTemplate = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface WelcomeTemplateProps {
+  name: string;
 }
 
-export default WelcomeTemplate
+const WelcomeTemplate = (props: WelcomeTemplateProps) => {
+  return (
+    <div>
+      <Html>
+        <Tailwind>
+          <Body className="bg-white">
+            <Container>
+              <Text className="text-2xl font-bold">Hello {props.name}</Text>
+              <Link href="https://www.google.com">Google</Link>
+            </Container>
+          </Body>
+        </Tailwind>
+      </Html>
+    </div>
+  );
+};
+
+const body: CSSProperties = {
+  backgroundColor: "#FFF",
+};
+const heading: CSSProperties = {
+  fontSize: "24px",
+};
+
+export default WelcomeTemplate;
